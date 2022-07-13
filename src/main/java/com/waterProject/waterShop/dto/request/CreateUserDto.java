5509912904ad8ea5a4d9566e2306218d58709195
IlyaYanakov <104890933+IlyaYanakov@users.noreply.github.com>
2022-07-13 17:18:54 +0300

    Dto and Repository
    Добавил пакеты dto и repository

---
+++ src/main/java/com/waterProject/waterShop/dto/request/CreateUserDto.java
@@ -0,0 +1,15 @@
+package com.waterProject.waterShop.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CreateUserDto {
+    Integer cityId;
+}
---
+++ src/main/java/com/waterProject/waterShop/repository/MessagesRepository.java
@@ -0,0 +1,8 @@
+package com.waterProject.waterShop.repository;
+
+import com.waterProject.waterShop.domain.Messages;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessagesRepository extends JpaRepository<Messages, Long> {
+
+}
